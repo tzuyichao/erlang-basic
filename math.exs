7d5984bed64b4c4f79f3e0dc110358ec970118b6
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-20 11:22:30 +0800

    ithelp example

--- math.exs
+++ math.exs
@@ -5,6 +5,9 @@ defmodule Math do
   
   def sum([]), do: 0
   def sum([head | tail]), do: head + sum(tail)
+
+  def sum_tail([], total), do: total
+  def sum_tail([head | tail], total), do: sum_tail(tail, head + total)
 end
 
 y = Math.add_one(10)
@@ -12,5 +15,8 @@ IO.inspect(y)
 
 z = Math.sum([1, 2, 3, 4, 5, 6])
 IO.inspect(z)
+
+a = Math.sum_tail([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)
+IO.inspect(a)
 
 System.halt()
