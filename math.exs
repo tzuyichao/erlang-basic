f70580bedc4950df6283b525c47543797710210a
TERENCE.CHAO <terence.chao@deltaww.com>
2020-03-20 11:14:12 +0800

    ithelp example

--- math.exs
+++ math.exs
@@ -2,9 +2,15 @@ defmodule Math do
   def add_one(x) do
     x + 1    
   end
+  
+  def sum([]), do: 0
+  def sum([head | tail]), do: head + sum(tail)
 end
 
 y = Math.add_one(10)
 IO.inspect(y)
+
+z = Math.sum([1, 2, 3, 4, 5, 6])
+IO.inspect(z)
 
 System.halt()
